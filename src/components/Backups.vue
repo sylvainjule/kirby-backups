df5266b21948b3af29631f12acb9696913d066b5
Bastian Allgeier <mail@bastianallgeier.com>
2021-09-27 17:51:28 +0200

    Migrate to 3.6

--- src/components/Backups.vue
+++ src/components/Backups.vue
@@ -1,145 +1,125 @@
 <template>
+  <k-inside>
     <k-view class="k-backups-view">
-        <k-header class="k-backups-view-header">
-            <span v-if="listLoading">{{ $t('view.backups') }}</span>
-            <span v-else>{{ backups.length }} {{ $t('backups.pluralized', {}, backups.length) }}</span>
+      <k-header class="k-backups-view-header">
+        {{ title }}
 
-            <k-button-group slot="left">
-                <k-button v-if="creationStatus == 'default'" icon="add" @click="createBackup">{{ $t('backups.create') }}</k-button>
-                <k-button v-else-if="creationStatus == 'progress'" icon="backupsLoader" :disabled="true">{{ $t('backups.create.process') }}</k-button>
-                <k-button v-else-if="creationStatus == 'success'" icon="check" :disabled="true" theme="positive">{{ $t('backups.create.success') }}</k-button>
-                <k-button v-else-if="creationStatus == 'error'" icon="alert" :disabled="true" theme="negative">{{ $t('backups.create.error') }}</k-button>
-            </k-button-group>
+        <k-button-group slot="left">
+          <k-button v-if="creationStatus == 'default'" icon="add" @click="createBackup">{{ $t('backups.create') }}</k-button>
+          <k-button v-else-if="creationStatus == 'progress'" icon="backupsLoader" :disabled="true">{{ $t('backups.create.process') }}</k-button>
+          <k-button v-else-if="creationStatus == 'success'" icon="check" :disabled="true" theme="positive">{{ $t('backups.create.success') }}</k-button>
+          <k-button v-else-if="creationStatus == 'error'" icon="alert" :disabled="true" theme="negative">{{ $t('backups.create.error') }}</k-button>
+        </k-button-group>
 
-            <k-button-group slot="right" v-if="backups.length">
-                <k-button icon="download" @click="copyAndDownload('latest')" v-if="downloading !== 'latest'">{{ $t('backups.download.latest') }}</k-button>
-                <k-button icon="backupsLoader" :disabled="true" v-else>{{ $t('backups.downloading') }}</k-button>
-                <k-button icon="trash" @click="openBackupsDeleteDialog">{{ $t('backups.delete.some') }}</k-button>
-            </k-button-group>
-        </k-header>
+        <k-button-group slot="right" v-if="backups.length">
+          <k-button icon="download" @click="copyAndDownload('latest')" v-if="downloading !== 'latest'">{{ $t('backups.download.latest') }}</k-button>
+          <k-button icon="backupsLoader" :disabled="true" v-else>{{ $t('backups.downloading') }}</k-button>
+          <k-button icon="trash" @click="openBackupsDeleteDialog">{{ $t('backups.delete.some') }}</k-button>
+        </k-button-group>
+      </k-header>
 
-        <section class="backups-section" v-if="backups.length">
-            <header class="backups-header">
-                <div class="backup-filename">{{ $t('backups.filename') }}</div>
-                <div class="backup-size">{{ $t('backups.size') }}</div>
-                <div class="backup-date">{{ $t('backups.created') }}</div>
-            </header>
-            <ul class="backups-list">
-                <backup-entry v-for="backup in backups" :key="backup.filename" :backup="backup" :downloading="downloading == backup.filename" @download="copyAndDownload" @delete="openBackupDeleteDialog(backup)" />
-            </ul>
-        </section>
-        <div class="backups-placeholder" v-else>
-            <div class="backups-placeholder-loader" v-if="listLoading"><k-icon type="backupsLoader" /></div>
-            <div class="backups-placeholder-empty" v-else>{{ $t('backups.placeholder') }}</div>
-        </div>
+      <section class="backups-section" v-if="backups.length">
+        <header class="backups-header">
+          <div class="backup-filename">{{ $t('backups.filename') }}</div>
+          <div class="backup-size">{{ $t('backups.size') }}</div>
+          <div class="backup-date">{{ $t('backups.created') }}</div>
+        </header>
+        <ul class="backups-list">
+          <backup-entry v-for="backup in backups" :key="backup.filename" :backup="backup" :downloading="downloading == backup.filename" @download="copyAndDownload" @delete="openBackupDeleteDialog(backup)" />
+        </ul>
+      </section>
+      <div class="backups-placeholder" v-else>
+        <div class="backups-placeholder-empty" v-else>{{ $t('backups.placeholder') }}</div>
+      </div>
 
-        <backup-delete-dialog ref="backup-delete" @deleted="onBackupDeleted"/>
-        <backups-delete-dialog ref="backups-delete" @deleted="onBackupsDeleted" :backups="backups"/>
+      <backup-delete-dialog ref="backup-delete" @deleted="$reload"/>
+      <backups-delete-dialog ref="backups-delete" @deleted="$reload" :backups="backups"/>
     </k-view>
+  </k-inside>
 </template>
 
 <script>
-import BackupEntry         from './BackupEntry.vue'
-import BackupDeleteDialog  from './BackupDeleteDialog.vue'
-import BackupsDeleteDialog from './BackupsDeleteDialog.vue'
+  import BackupEntry         from './BackupEntry.vue'
+  import BackupDeleteDialog  from './BackupDeleteDialog.vue'
+  import BackupsDeleteDialog from './BackupsDeleteDialog.vue'
 
-export default {
+  export default {
     components: {
-        'backup-entry': BackupEntry,
-        'backup-delete-dialog': BackupDeleteDialog,
-        'backups-delete-dialog': BackupsDeleteDialog,
+      'backup-entry': BackupEntry,
+      'backup-delete-dialog': BackupDeleteDialog,
+      'backups-delete-dialog': BackupsDeleteDialog,
+    },
+    props: {
+        backups: Array,
+        title: String
     },
     data() {
-        return {
-            backups: [],
-            listLoading: true,
-            downloading: false,
-            creationStatus: 'default',
-        }
+      return {
+        downloading: false,
+        creationStatus: 'default',
+      }
     },
     mounted() {
-        this.getBackups();
-        window.beforeunload = this.deletePublicBackups();
+      window.beforeunload = this.deletePublicBackups();
     },
     destroyed() {
-        this.deletePublicBackups();
+      this.deletePublicBackups();
     },
     methods: {
-        getBackups() {
-            this.$api.get('backups/get-backups-list')
-                .then(response => {
-                    this.backups = response
-                    this.listLoading = false
-                })
-        },
-        deletePublicBackups() {
-            this.$api.post('backups/delete-public-backups');
-        },
-        createBackup() {
-            this.creationStatus = 'progress';
+      deletePublicBackups() {
+        this.$api.post('backups/delete-public-backups');
+      },
+      async createBackup() {
+        this.creationStatus = 'progress';
 
-            this.$api.get('plugin-janitor/backupZip')
-                .then(response => {
-                    this.setCreationStatus(response.status, true)
+        try {
+            const response = await this.$api.get('plugin-janitor/backupZip');
+            await this.$reload();
+            this.setCreationStatus(response.status, true);
+        } catch(error) {
+            this.setCreationStatus('error', true);
+        }
+      },
+      setCreationStatus(status, resetAfter = true) {
+        this.creationStatus = status == 200 ? 'success' : 'error';
 
-                    if(response.status == 200) {
-                        let newBackup = {
-                            filename: response.filename + '.zip',
-                            size: response.nicesize,
-                            date: response.modified
-                        }
+        if(resetAfter) {
+          setTimeout(() => {
+            this.creationStatus = 'default'
+          }, 2000);
+        }
+      },
 
-                        this.backups.unshift(newBackup)
-                    }
-                })
-                .catch(error => {
-                    this.setCreationStatus('error', true);
-                })
-        },
-        setCreationStatus(status, resetAfter = true) {
-            this.creationStatus = status == 200 ? 'success' : 'error';
+      /* Download
+      -------------------*/
 
-            if(resetAfter) {
-                setTimeout(() => {
-                    this.creationStatus = 'default'
-                }, 2000);
-            }
-        },
+      async copyAndDownload(filename) {
+        this.downloading = filename
 
-        /* Download
-        -------------------*/
+        filename = filename == 'latest' ? this.backups[0].filename : filename
 
-        copyAndDownload(filename) {
-            this.downloading = filename
+        const response = await this.$api.get('backups/copy-to-assets', {filename: filename})
 
-            filename = filename == 'latest' ? this.backups[0].filename : filename
+        if (response.url) {
+            window.location = response.url
+        }
 
-            this.$api.get('backups/copy-to-assets', {filename: filename})
-                .then(response => {
-                    if(response.url) window.location = response.url
-                    this.downloading = false
-                })
-        },
+        this.downloading = false
+      },
 
-        /* Dialogs
-        -------------------*/
+      /* Dialogs
+      -------------------*/
 
-        openBackupDeleteDialog(backup) {
-            this.$refs['backup-delete'].open(backup)
-        },
-        onBackupDeleted(filename) {
-            this.backups = this.backups.filter(el => el.filename !== filename)
-        },
-        openBackupsDeleteDialog() {
-            this.$refs['backups-delete'].open()
-        },
-        onBackupsDeleted(filenames) {
-            this.backups = this.backups.filter(el => !filenames.includes(el.filename));
-        },
+      openBackupDeleteDialog(backup) {
+        this.$refs['backup-delete'].open(backup)
+      },
+      openBackupsDeleteDialog() {
+        this.$refs['backups-delete'].open()
+      }
     }
-};
+  };
 </script>
 
 <style lang="scss">
-    @import '../assets/css/styles.scss'
+  @import '../assets/css/styles.scss';
 </style>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import Backups from './components/Backups.vue';
 
 panel.plugin('sylvainjule/backups', {
-    views: {
-        backups: {
-            icon: 'backups',
-            component: Backups,
-        }
-    },
-    icons: {
-        backups: '<path d="M15.33,6.67H.67A.67.67,0,0,1,0,6V.67A.67.67,0,0,1,.67,0H15.33A.67.67,0,0,1,16,.67V6A.67.67,0,0,1,15.33,6.67Zm-14-1.33H14.67v-4H1.33Zm14,9.33H.67A.67.67,0,0,1,0,14V8.67A.67.67,0,0,1,.67,8H15.33a.67.67,0,0,1,.67.67V14A.67.67,0,0,1,15.33,14.67Zm-14-1.33H14.67v-4H1.33Zm5.33-10A.67.67,0,0,0,6,2.67H3.33A.67.67,0,0,0,3.33,4H6A.67.67,0,0,0,6.67,3.33Zm0,8A.67.67,0,0,0,6,10.67H3.33a.67.67,0,0,0,0,1.33H6A.67.67,0,0,0,6.67,11.33Zm6-8.67a.67.67,0,1,1-.67.67A.67.67,0,0,1,12.67,2.67Zm0,8a.67.67,0,1,1-.67.67A.67.67,0,0,1,12.67,10.67Z"/>',
-        backup: '<path d="M7.12,11.86a.56.56,0,0,1-.4-.16l-2-2a.56.56,0,0,1,.79-.79L7.12,10.5l3.56-3.56a.56.56,0,0,1,.79.79l-4,4A.56.56,0,0,1,7.12,11.86Z"/><path d="M14,15.45H2.17A2.17,2.17,0,0,1,0,13.28V1.41A.69.69,0,0,1,.69.72H6.13a.69.69,0,0,1,.59.33L8,3.19h7.52a.69.69,0,0,1,.69.69v9.4A2.17,2.17,0,0,1,14,15.45ZM1.38,2.1V13.28a.8.8,0,0,0,.79.79H14a.8.8,0,0,0,.8-.79V4.57H7.61A.69.69,0,0,1,7,4.23L5.74,2.1Z"/>',
-        backupsLoader: '<g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="1.75"><circle cx="7" cy="7" r="7.2" stroke="#000" stroke-opacity=".2"/><path d="M14.2,7c0-4-3.2-7.2-7.2-7.2" stroke="#000"><animateTransform attributeName="transform" type="rotate" from="0 7 7" to="360 7 7" dur="1s" repeatCount="indefinite"/></path></g></g>'
-    }
+  components: {
+    "k-backups-view": Backups,
+  },
+  icons: {
+    backup: '<path d="M7.12,11.86a.56.56,0,0,1-.4-.16l-2-2a.56.56,0,0,1,.79-.79L7.12,10.5l3.56-3.56a.56.56,0,0,1,.79.79l-4,4A.56.56,0,0,1,7.12,11.86Z"/><path d="M14,15.45H2.17A2.17,2.17,0,0,1,0,13.28V1.41A.69.69,0,0,1,.69.72H6.13a.69.69,0,0,1,.59.33L8,3.19h7.52a.69.69,0,0,1,.69.69v9.4A2.17,2.17,0,0,1,14,15.45ZM1.38,2.1V13.28a.8.8,0,0,0,.79.79H14a.8.8,0,0,0,.8-.79V4.57H7.61A.69.69,0,0,1,7,4.23L5.74,2.1Z"/>',
+    backupsLoader: '<g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="1.75"><circle cx="7" cy="7" r="7.2" stroke="#000" stroke-opacity=".2"/><path d="M14.2,7c0-4-3.2-7.2-7.2-7.2" stroke="#000"><animateTransform attributeName="transform" type="rotate" from="0 7 7" to="360 7 7" dur="1s" repeatCount="indefinite"/></path></g></g>'
+  }
 });
